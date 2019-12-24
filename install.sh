6556840a246d981ad85fdd8c36b213c511bd0495
ypn007 <55645320+ypn007@users.noreply.github.com>
2019-12-24 03:16:00 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#Coded By ;-Yell Phone Naing
+#Start Of Setting Up
+echo "Setting Up Files" | lolcat -a -d 15
+cp -r termux.zip /data/data/com.termux/files/usr/share
+cp Y-Tools /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+rm *
+chmod +x Y-Tools
+echo "Finish,To Start My Tool Type (Y-Tools)
+#End Of Setting Up
