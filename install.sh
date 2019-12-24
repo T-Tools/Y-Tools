c5c6fb2ff245af35e89ceb1d630b05f5e9c5bde7
ypn007 <55645320+ypn007@users.noreply.github.com>
2019-12-24 03:34:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo "Setting Up Files" | lolcat -a -d 15
 cp -r termux.zip /data/data/com.termux/files/usr/share
 cp Y-Tools /data/data/com.termux/files/usr/bin
-cd /data/data/com.termux/files/usr/bin
 rm *
+cd /data/data/com.termux/files/usr/bin
 chmod +x Y-Tools
 echo "Finish,To Start My Tool Type (Y-Tools)
 #End Of Setting Up
